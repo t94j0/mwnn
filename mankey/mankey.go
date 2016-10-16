4644b524e41af36f5c0fe55af8a92d087032e1b6
woodywood117 <darkgalac@gmail.com>
2016-10-15 23:21:41 -0400

    Started writing key generation function, need to have it write them to files

--- mankey/mankey.go
+++ mankey/mankey.go
@@ -4,6 +4,7 @@ import(
 	"fmt"
 	"os"
 	"strings"
+	"github.com/howeyc/gopass"
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
 )
@@ -11,13 +12,37 @@ import(
 var RecievedBadName = errors.New("Names cannot contain \"()<>|\x00\"")
 var RecievedBadEmail = errors.New("Email cannot contain \"()<>|\x00\"")
 var RecievedBadComment = errors.New("Comment cannot contain \"()<>|\x00\"")
+var PasswordMismatch = errors.New("Input passwords do not match")
 
-func generateKeyPair(pubKeyLoc, privKeyLoc, name, email, comment string) error {
+
+func generateKeyPair(pubKeyLoc, privKeyLoc, name, comment, email string) error {
 
 	if strings.ContainsAny(name, "()<>|\x00") {
 		return RecievedBadName
 	}
-	openpgp.NewEntity(
+	if strings.ContainsAny(email, "()<>|\x00") {
+		return RecievedBadEmail
+	}
+	if strings.ContainsAny(comment, "()<>|\x00") {
+		return RecievedBadComment
+	}
+	fmt.Printf("Private Key Password: ")
+	privateKeyPass, err = gopass.GetPasswd()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Re-enter Private Key Password: ")
+	privateKeyPass1, err = gopass.GetPasswd()
+	if err != nil {
+		return err
+	}
+	if privateKeyPass != privateKeyPass1 {
+		return PasswordMismatch
+	}
+	if newPair, err := openpgp.NewEntity(name, comment, email, nil); err != nil {
+		return err
+	}
+	// TODO Encode keys with armor.Encode() and write to files
 }
 
 func editKeyName() {
