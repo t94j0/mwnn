16460e97e591a41643c5f76abe2763c01ce81bfa
Max Harley <augeocacher@gmail.com>
2016-10-17 13:55:18 -0400

    Update key management

--- keys/keys.go
+++ keys/keys.go
@@ -1,9 +1,11 @@
-package mankey
+package keys
 
-import(
+import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
+
 	"github.com/howeyc/gopass"
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
@@ -13,7 +15,6 @@ var RecievedBadName = errors.New("Names cannot contain \"()<>|\x00\"")
 var RecievedBadEmail = errors.New("Email cannot contain \"()<>|\x00\"")
 var RecievedBadComment = errors.New("Comment cannot contain \"()<>|\x00\"")
 var PasswordMismatch = errors.New("Input passwords do not match")
-
 
 func generateKeyPair(pubKeyLoc, privKeyLoc, name, comment, email string) error {
 
@@ -27,22 +28,48 @@ func generateKeyPair(pubKeyLoc, privKeyLoc, name, comment, email string) error {
 		return RecievedBadComment
 	}
 	fmt.Printf("Private Key Password: ")
-	privateKeyPass, err = gopass.GetPasswd()
+	privateKeyPass, err := gopass.GetPasswd()
 	if err != nil {
 		return err
 	}
 	fmt.Printf("Re-enter Private Key Password: ")
-	privateKeyPass1, err = gopass.GetPasswd()
+	privateKeyPass1, err := gopass.GetPasswd()
 	if err != nil {
 		return err
 	}
-	if privateKeyPass != privateKeyPass1 {
+	if string(privateKeyPass) != string(privateKeyPass1) {
 		return PasswordMismatch
 	}
-	if newPair, err := openpgp.NewEntity(name, comment, email, nil); err != nil {
+	newPair, err := openpgp.NewEntity(name, comment, email, nil)
+	if err != nil {
 		return err
 	}
 	// TODO Encode keys with armor.Encode() and write to files
+	for _, identity := range newPair.Identities {
+		if err := identity.SelfSignature.SignUserId(identity.UserId.Id, newPair.PrimaryKey, newPair.PrivateKey, nil); err != nil {
+			return err
+		}
+	}
+
+	// Create public key file
+	publicKeyFile, err := os.Create(pubKeyLoc)
+	if err != nil {
+		return err
+	}
+	armor.Encode(publicKeyFile, openpgp.PublicKeyType, nil)
+	defer publicKeyFile.Close()
+	newPair.Serialize(publicKeyFile)
+
+	// Create private key file
+	privateKeyFile, err := os.Create(privKeyLoc)
+	if err != nil {
+		return err
+	}
+	armor.Encode(privateKeyFile, openpgp.PrivateKeyType, nil)
+	defer privateKeyFile.Close()
+	newPair.Serialize(privateKeyFile)
+
+	return nil
 }
 
 func editKeyName() {
@@ -53,6 +80,6 @@ func editKeyComment() {
 
 }
 
-func changeDefaultKey(){
+func changeDefaultKey() {
 
 }
